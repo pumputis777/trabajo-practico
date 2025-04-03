d5dc0ecec38ab791d2dddb732c7c03fde9d8a9c5
pumputis777 <facundopumputis@gmail.com>
2025-04-03 08:58:31 -0300

    Add files via upload

---
+++ trabajo practico.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int num1;
+int num2;
+int suma;
+int resta;
+int multiplicacion;
+int division;
+cout<<"ingrese el primer numero: ";
+cin>> num1;
+cout<<"ingrese el segundo numero: ";
+cin>> num2;
+suma = num1 + num2;
+multiplicacion= num1 * num2;
+resta = num1 - num2; 
+division = num1 / num2;
+cout<<"este es el resultado de la suma: " <<suma<<endl;
+cout<<"este es el resultado de la resta: " <<resta<<endl;
+cout<<"este es el resultado de la multiplicacion: " <<multiplicacion<<endl;
+cout<<"este es el resultado de la division: " <<division<<endl;
+    return 0;
+}
