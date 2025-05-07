24460cc7785fb9c3d9f0e8c25b1140524ef25b7a
pumputis777 <facundopumputis@gmail.com>
2025-05-07 19:41:45 -0300

    trabajo practico.cpp

--- trabajo practico.cpp
+++ trabajo practico.cpp
@@ -17,18 +17,22 @@ int suma;
 int resta;
 int multiplicacion;
 int division;
+  
 cout<< "este programa suma,resta,multiplica y divide dos numeros" <<endl;
 cout<<"ingrese el primer numero: ";
 cin>> num1;
 cout<<"ingrese el segundo numero: ";
 cin>> num2;
+  
 suma = num1 + num2;
 multiplicacion= num1 * num2;
 resta = num1 - num2; 
 division = num1 / num2;
+  
 cout<<"este es el resultado de la suma: " <<suma<<endl;
 cout<<"este es el resultado de la resta: " <<resta<<endl;
 cout<<"este es el resultado de la multiplicacion: " <<multiplicacion<<endl;
 cout<<"este es el resultado de la division: " <<division<<endl;
+  
     return 0;
 }
